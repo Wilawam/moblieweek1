251d97e29f9741441c07a58897ebf103e530aad3
wilawan <s6302041510109@email.kmutnb.ac.th>
2021-08-05 17:14:36 +0700

    เรียนเรื่อง fx

--- dart/week2.dart
+++ dart/week2.dart
@@ -1,5 +1,6 @@
 void main() {
 //print(callGen(1991));
+//เรียกใช้ funtion
 callWeb("Google" , protocal: "https" , port : 443);
 }
  // var year = 2000;
@@ -11,6 +12,7 @@ callWeb("Google" , protocal: "https" , port : 443);
 // การเช็คเงื่อนไข           True         False
  String callGen(int year) => (year >=1996) ? ("Gen Z") : ("Gen Y") ;
 
+//การสร้าง funtion การแสดงข้อมูลในตัวแปร list โดยใช้ for in และ return จำนวนข้อมูลใน list กลับด้วย
 int callColor([String newColor = "pink"]) {
   List<dynamic> color = ["red", "blue", "green"];
   color.add("pink");
@@ -21,7 +23,7 @@ int callColor([String newColor = "pink"]) {
  return color.length;
 }
 
-
+//การสร้าง funtion การแสดงข้อมูลการเชื่อมต่อเว็บและ port แบบ Allow 
 void callWeb(String web, {int port = 80, String protocal = "https"}) =>
 print("$protocal $web $port");
 
